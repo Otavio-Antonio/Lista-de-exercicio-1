1a6304d92db3f537cd738e0212945452ea5a0933
Otavio-Antonio <71262689+Otavio-Antonio@users.noreply.github.com>
2021-02-19 18:19:02 -0300

    Update main.c

--- L1Q8/main.c
+++ L1Q8/main.c
@@ -3,9 +3,11 @@
 int main(){
 int vet[] = {4,9,13};
 int i;
+     // b
      for(i=0;i<3;i++){
           printf("%X ",vet+i);
      }
+     // a
      for(i=0;i<3;i++){
           printf("%d ",*(vet+i));
      }
